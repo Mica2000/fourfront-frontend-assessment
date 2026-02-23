64b266473c77cf628978f9bb299ec498e19c2084
Mica2000 <davidatmaina@gmail.com>
2026-02-23 19:45:07 +0300

    Created html Layout and added Images

---
+++ front-end/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.foundation-btn').addEventListener('click', function() {
+  document.querySelectorAll('.membership-content')[0].classList.toggle('d-none');
+});
+
+document.querySelector('.economy-btn').addEventListener('click', function() {
+  document.querySelectorAll('.membership-content')[1].classList.toggle('d-none');
+});
